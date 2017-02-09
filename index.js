418243e7f606d0fafb764b0503b9a29caa03af73
Jamie Lacivita <JCLacivita@gmail.com>
2017-02-08 20:16:20 -0700

    added more star and planet data

--- index.js
+++ index.js
@@ -32,6 +32,12 @@ db.stars.push(s01)
 let s02 = {name:"star02", description:"this is the descriptoin of start02", galaxyId:"1", id:"2"}
 db.stars.push(s02)
 
+let p01 = {name:"planet01", description:"this is the description of a planet01", galaxyId:"1", starId:"1", id:"1"}
+db.planets.push(p01)
+
+let p02 = {name:"planet02", description:"this is the description of a planet02", galaxyId:"1", starId:"1", id:"2"}
+db.planets.push(p02)
+
 //function to craete a galaxy
 server.post('/galaxies',function(req,res){
     //console.log("A post function has been made to the root of the server.")
@@ -39,6 +45,7 @@ server.post('/galaxies',function(req,res){
 
     galaxy = req.body;
     galaxy.id = uuid.v1()
+    galaxy.planets = []
     db.galaxies.push(galaxy)
 
     res.send({message: "galaxy succesfully created.", data: galaxy})
@@ -108,13 +115,11 @@ server.delete('/galaxies/:id',function(req,res){
         {
             star = db.stars[i]
             console.log("galaxyId:", galaxyId)
-            console.log("indexToRemove:", indexToRemove)
-            if (star.galaxyId == i)
+            if (star.galaxyId == found_galaxy.id)
             {   
-                console.log("we are removing a star.")
+                console.log("we are removing a star with a galaxy id of: ", star.galaxyId)
                 db.stars.splice(i,1)
             } 
-
         }
 
         return res.send({message:"galaxy has been remvoed.",data: db.galaxies})
@@ -124,7 +129,7 @@ server.delete('/galaxies/:id',function(req,res){
 
 //-- section for stars --//
 
-//function to craete a star
+//function to create a star
 server.post('/stars/:galaxyId',function(req,res){
 
     //verify that we are placing the star in a valid galaxy.
@@ -137,6 +142,8 @@ server.post('/stars/:galaxyId',function(req,res){
         star.galaxyId = req.params.galaxyId;
         star.id = uuid.v1()
         db.stars.push(star)
+        
+        found_galaxy.planets.push({starId:star.id})
         return res.send({message: "star succesfully created.", data: db.stars})
     }
     else
@@ -211,7 +218,87 @@ server.delete('/stars/:id',function(req,res){
     return res.send("No star by that id!  Nothing has been removed.")
 })
 
+//-- section for stars --//
+
+//function to create a planet
+server.post('/planets/:starId',function(req,res){
+
+    //verify that we are placing the planet around a valid star.
+    let starId = req.params.starId;    
+    let found_star = db.stars.find(star => {return star.id == starId})
+
+    if (found_star)
+    {
+        planet = req.body;
+        planet.starId = req.params.starId;
+        planet.id = uuid.v1()
+        planet.galaxyId = found_star.galaxyId
+        db.planets.push(planet)
+        return res.send({message: "planet succesfully created.", data: db.planets})
+    }
+    else
+    {
+        return res.send({message: "That star DNE"})
+    }
+
+})
+
+//function to return all planets.
+server.get('/planets',function(req,res){
+    res.send({data: db.planets})
+})
+
+//function to return a single planet by an id. 
+server.get('/planets/:id',function(req,res){
+
+    let planetId = req.params.id;
+    let found_planet = db.planets.find(planet => {return planet.id == planetId})
+
+    if (found_planet)
+    {
+        return res.send({data: found_planet})
+    }
+
+    return res.send("No planet with that id!")
+})
+
+//function to edit a single planet by an id. 
+server.put('/planets/:id',function(req,res){
+
+    let edited_planet = req.body;   //this the star that was passed to us for editing. 
+    let planetId = req.params.id;  //this will be the id of both the new and old star.
+
+    let found_planet = db.planets.find(planet => {return planet.id == planetId})
+
+    if (found_planet)
+    {
+        //add the old id to the planet object that was passed to us.
+        edited_planet.id = found_planet.id;
+        edited_planet.starId = found_planet.starId;
+        edited_planet.galaxyId = found_planet.galaxyId;
+
+        //remove the old object from the array.
+        //Find its index.
+
+        let indexToRemove = db.planets.indexOf(found_planet)
+        db.planets.splice(indexToRemove,1)
+        db.planets.push(edited_planet)
+
+        //return the edited object to the user.
+        return res.send({data: edited_planet})
+
+    }
+
+    return res.send("No planet by that id!")
+})
+
 server.listen(PORT, function(){console.log("the server is runing confortabally.")})
 
-//ToDo: if you remove a galaxy you need to remove the stars within it so as not to abandon them.
+//Done: if you remove a galaxy you need to remove the stars within it so as not to abandon them.
+
+//ToDo: function to create a planet
+//ToDo: function to remove a planet
+//ToDo: function to show all a planets
+//ToDo: function to show a single planet by id.
+//ToDo: function to delete a planet.
 
